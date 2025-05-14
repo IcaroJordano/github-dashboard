cb763b6c7c870cfc154770de777028eae8ee2ec2
Icaro Jordano <Icarojordano>
2025-05-14 11:57:23 -0300

    fix: teste com HashRouter

--- src/pages/Rotas.jsx
+++ src/pages/Rotas.jsx
@@ -1,14 +1,16 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Overview from "./Overview";
 import Repositorio from "./Repositorios";
 import Deploys from "./Deploys";
 
 export function Rotas() {
   return (
-    <Routes>
-      <Route path="/github-dashboard/" element={<Overview />}></Route>
-      <Route path="/github-dashboard/repositorios" element={<Repositorio/>}></Route>
-      <Route path="/github-dashboard/deploys" element={<Deploys/>}></Route>
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Overview />}></Route>
+        <Route path="/repositorios" element={<Repositorio />}></Route>
+        <Route path="/deploys" element={<Deploys />}></Route>
+      </Routes>
+    </HashRouter>
   );
 }
